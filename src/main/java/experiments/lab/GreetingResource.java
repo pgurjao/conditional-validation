79958df1a34d792ef1fe986f7d2f3ea297af7164
Paulo Gurjão <4331166+pgurjao@users.noreply.github.com>
2023-09-22 00:26:21 -0300

    refactor response

--- src/main/java/experiments/lab/GreetingResource.java
+++ src/main/java/experiments/lab/GreetingResource.java
@@ -8,14 +8,15 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @Path("/hello")
 public class GreetingResource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public String hello(@Valid TestConditionalValidationDTO dto) throws JsonProcessingException {
+    public Response hello(@Valid TestConditionalValidationDTO dto) throws JsonProcessingException {
     	String response = new ObjectMapper().writeValueAsString(dto);
-    	return response;
+    	return Response.ok(response).build();
     }
 }
